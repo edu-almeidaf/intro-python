77d060da231a6409f44efdbc3cdc16782946a2ab
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-09-19 23:51:11 -0300

    feat: feito os exercícios do dia

--- src/average.py
+++ src/average.py
@@ -2,4 +2,7 @@ from typing import List
 
 
 def find_average(numbers: List[int]) -> float:
-    raise NotImplementedError
+    total = 0
+    for number in numbers:
+        total += number
+    return total / len(numbers)
